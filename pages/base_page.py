b5ebe1981708a75fbd4d63f311cfc59381cf5f08
DaniilSh23 <94797832+DaniilSh23@users.noreply.github.com>
2022-05-16 11:26:14 +0400

    'modification_of_test_and_abstract_classes'

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         '''
         Конструктор базового, абстрактного класса
         :param browser: объект браузера
@@ -7,6 +9,7 @@ class BasePage():
         '''
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         '''
@@ -14,3 +17,16 @@ class BasePage():
         :return: None
         '''
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        '''
+        Метод проверки наличия элемента
+        :param how:  как искать (css, id, xpath и тд)
+        :param what: что искать (передаётся строка-селектор)
+        :return: bool
+        '''
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,15 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    '''
+    Тест, который проверяет наличие ссылки для логина на странице
+    :param browser: фикстура - драйвер браузера
+    :return: None
+    '''
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
